d066599a6f4bdeafce51aea336765e14cdd70b0f
JhonattanAron <aron.cachago@gmail.com>
2025-08-12 12:48:05 -0500

    1.1.6

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "http://localhost:3000", // frontend permitido
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // si vas a enviar cookies o auth headers
+  });
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
