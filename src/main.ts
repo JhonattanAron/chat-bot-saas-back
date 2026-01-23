5724bf4458275477e47afbe40c8e405cf9f4be26
JhonattanAron <aron.cachago@gmail.com>
2026-01-22 19:00:53 -0500

    2.3 -> update cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ["http://localhost:3000", "https://api.aurentric.com"],
+    origin: [
+      "http://localhost:3000",
+      "https://www.aurentric.com",
+      "https://aurentric.com",
+    ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });
