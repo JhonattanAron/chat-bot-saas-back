4ec43342bd10df1200e468afecc319be63a95e3c
JhonattanAron <aron.cachago@gmail.com>
2026-01-27 19:49:09 -0500

    2.6.2

--- src/modules/whastAppQr/whatsapp.controller.ts
+++ src/modules/whastAppQr/whatsapp.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Post, Body, Query, Get } from "@nestjs/common";
+import {
+  Controller,
+  Post,
+  Body,
+  Query,
+  Get,
+  InternalServerErrorException,
+} from "@nestjs/common";
 import { WhatsappService } from "./whatsapp.service";
 import { SendMessageDto } from "./dto/send-message.dto";
 import { SendBulkDto } from "./dto/send-bulk.dto";
@@ -21,18 +28,29 @@ export class WhatsappController {
     const session = this.service.getSession(userId);
     return { connected: !!session }; // true si la sesión existe
   }
+  // pseudo-controlador
   @Post("start-session")
   async startSession(@Body() body: { userId: string }) {
     const { userId } = body;
 
-    return new Promise<{ qr: string }>((resolve, reject) => {
-      try {
-        this.service.startSession(userId, (qr) => {
-          resolve({ qr });
-        });
-      } catch (err) {
-        reject(err);
+    try {
+      // si ya hay socket en memoria
+      if (this.service.isConnected(userId)) {
+        return { qr: null, connected: true };
       }
-    });
+
+      let qrCode: string | null = null;
+      await this.service.startSession(userId, (qr) => {
+        qrCode = qr;
+      });
+
+      // si se genera QR, no está conectado todavía
+      return { qr: qrCode, connected: qrCode ? false : true };
+    } catch (err) {
+      console.error(err);
+      throw new InternalServerErrorException(
+        "Error al iniciar sesión WhatsApp",
+      );
+    }
   }
 }
